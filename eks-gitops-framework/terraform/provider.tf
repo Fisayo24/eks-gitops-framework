ee1f43a1be47414df15f56109424b5d31ff7d170
Fisayo24 <163774684+Fisayo24@users.noreply.github.com>
2026-05-21 07:06:32 -0400

    Update provider.tf

---
+++ eks-gitops-framework/terraform/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  # Locks down plugin versions to ensure environment stability
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" 
+}
